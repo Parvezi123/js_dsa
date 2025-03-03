0412fa2c92ec4a998a9a699c712e200e27d7218c
nidhidew <nidhicec97@gmail.com>
2025-03-03 22:47:04 +0530

    further more code for postfix and prefix incrmeent

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("hello world")
-
+// ----------------------------------------------------------------------------------------------------
 // variable and constants
 const accountId = 144553    // cant modify the value
 let accountEmail = "nidhi@google.com"
@@ -8,7 +8,6 @@ var accountPassword = "12345"
 accountEmail = "google.com"
 accountPassword = "78965"
 // accountCity = "delhi"
-
 console.table([accountId,accountEmail,accountPassword])
 let a = 20;
 function func(){
@@ -17,9 +16,34 @@ function func(){
 }
 func()
 console.log(a);
-
+// -------------------------------------------------------------------------------------------------------
+// type conversion
 let score = 33
-
 console.log(typeof score);
 let stringNum = String(score);
 console.log(typeof stringNum);
+
+// type conversion in string and number
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
+console.log(+true); //1
+
+// prefix increment
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+
+//postfix increment
+let x3 = 3;
+const y3 = x3++;
+// x is 4; y is 3
+
+let x4 = 3n;
+const y4 = x4++;
+// x2 is 4n; y2 is 3n
